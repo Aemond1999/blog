fafc8fa1ea5598037e30db7a1c8528972a13fb6b
Aemond1999 <127378981+Aemond1999@users.noreply.github.com>
2023-04-17 06:27:00 +0800

    增加DTO功能

---
+++ src/main/java/com/hya/blog/domain/dto/CommentDTO.java
@@ -0,0 +1,18 @@
+package com.hya.blog.domain.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+@Data
+@NoArgsConstructor
+public class CommentDTO {
+    private Long id;
+    private String content;
+    private Date createTime;
+    private Long createBy;
+    private Long toCommentUserId;
+    private String toCommentUserName;
+    private Long toCommentId;
+    private String UserName;
+}
---
+++ src/main/java/com/hya/blog/domain/dto/UserLoginDTO.java
@@ -0,0 +1,12 @@
+package com.hya.blog.domain.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class UserLoginDTO {
+    private Long id;
+    private String username;
+
+}
