d9e83d38c1dcfe1da7495156896aed2fea479228
hya <2643226300@qq.com>
2025-02-07 00:20:51 +0800

    添加查看评论功能

---
+++ blog-server/src/main/java/com/hya/common/vo/LoginUserVo.java
@@ -0,0 +1,21 @@
+package com.hya.common.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+/**
+ * 用户个人数据
+ */
+public class LoginUserVo {
+    private Long id;
+    private String account;
+    private String avatar;
+    private String email;
+    private String nickname;
+}
